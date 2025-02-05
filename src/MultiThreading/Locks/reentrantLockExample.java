9e93f61928367dde90f47ac926e44b5dc17a5803
Gautam <151940634+bitEngineer09@users.noreply.github.com>
2025-02-05 22:03:20 +0530

    Update reentrantLockExample.java

--- src/MultiThreading/Locks/reentrantLockExample.java
+++ src/MultiThreading/Locks/reentrantLockExample.java
@@ -8,11 +8,11 @@ public class reentrantLockExample {
 //  yaha outer method ne lock acquire kiya,
 //  then innerMethod call hua, fir vo same lock ko acquire karne ki koshih kar ra hai
 //  this is a DEADLOCK condn.
-//  but as here is reentrant implementation class, to innermMethod again usi lock ko acquire kar sakta hai.
-//  reentrant matlab fir se enter
+//  but as here is reentrant implementation class, then innerMethod again usi lock ko acquire kar sakta hai.
+//  reentrant matlab fir se enter.
 //  jab reentrant lock use karte hai, to ek count maintain kiya jata hai
 //  matlab ki lock kitne baar acquire kiya gaya hai.
-//  agar extra unlocks laga diye to exception bhi aaega
+//  agar extra unlocks laga diye to exception bhi aaega.
 
     public void outerMethod() {
         lck.lock(); // 1) count - 1
