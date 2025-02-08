ff4224d9dc8704070bb6960964360519881400b2
bitEngineer09 <gautam.columbian@gmail.com>
2025-02-08 22:43:29 +0530

    8 Feb 25

---
+++ src/javaCollectionFramework/collectionframework/ConcurrentSkipListMapDemo.java
@@ -0,0 +1,9 @@
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class ConcurrentSkipListMapDemo {
+    public static void main(String[] args) {
+        ConcurrentSkipListMap<String,Integer> map = new ConcurrentSkipListMap<>();
+        map.put("Apple", 2);
+
+    }
+}
